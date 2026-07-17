f9350b6f8fcc14c931d869a97eccbf64834beb77
d0ul <support@pcor.me>
2026-07-17 11:28:52 +0900

    Initial Commit

---
+++ NowPlayingBar/NowPlayingBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingBarApp.swift
+//  NowPlayingBar
+//
+//  Created by 이도울 on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayingBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
